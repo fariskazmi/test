710cbaa5bd2359023b8cbe87e691db938592f6cc
fariskazmi <faris.kazmi2@gmail.com>
2020-03-23 22:58:54 -0600

    Updates

---
+++ precache-manifest.0a334da34fa73ed69fd69b0468c06f62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddca44b7bdaa5316d5e45c7a0062c0e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cabdfa28e470d900867",
+    "url": "/static/css/main.54c75bf2.chunk.css"
+  },
+  {
+    "revision": "d4199334dff9397f9063",
+    "url": "/static/js/2.ab8c833d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.ab8c833d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cabdfa28e470d900867",
+    "url": "/static/js/main.a44a1763.chunk.js"
+  },
+  {
+    "revision": "aaed884b9cd5f8f87494",
+    "url": "/static/js/runtime-main.97472ead.js"
+  }
+]);
